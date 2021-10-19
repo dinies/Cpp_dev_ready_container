de3a3ca27a7940ac5a85e48adc41737c652c7a52
dinies <edoardo.ghini@live.it>
2021-10-19 14:52:56 +0200

    added dockerfile and script for general purpose

---
+++ container_runner.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+CONTAINER_NAME=generic_container_1
+
+setfacl -m user:1000:r ${HOME}/.Xauthority
+dpkg -l | grep nvidia-container-toolkit &> /dev/null
+HAS_NVIDIA_TOOLKIT=$?
+which nvidia-docker > /dev/null
+HAS_NVIDIA_DOCKER=$?
+
+DOCKER_IMAGE_NAME="gui_dev_nvidia"
+DOCKER_BASE_IMAGE="nvidia/opengl:1.2-glvnd-devel-ubuntu18.04"
+if [ $HAS_NVIDIA_TOOLKIT -eq 0 ]; then
+  docker_version=`docker version --format '{{.Client.Version}}' | cut -d. -f1`
+  if [ $docker_version -ge 19 ]; then
+	  DOCKER_COMMAND="docker run --gpus all"
+  else
+	  DOCKER_COMMAND="docker run --runtime=nvidia"
+  fi
+elif [ $HAS_NVIDIA_DOCKER -eq 0 ]; then
+  DOCKER_COMMAND="nvidia-docker run"
+else
+  DOCKER_IMAGE_NAME="gui_dev_no_gpu"
+  DOCKER_BASE_IMAGE="ubuntu:bionic"
+  DOCKER_COMMAND="docker run"
+fi
+
+docker build -t $DOCKER_IMAGE_NAME \
+  --build-arg base_img=$DOCKER_BASE_IMAGE \
+  --build-arg git_email="$(git config user.email)" \
+  --build-arg git_name="$(git config user.name)" \
+  --build-arg ssh_prv_key="$(cat ~/.ssh/id_ed25519)" \
+  --build-arg ssh_pub_key="$(cat ~/.ssh/id_ed25519.pub)" \
+  --squash  .
+
+exec $DOCKER_COMMAND \
+     -it \
+     --name $CONTAINER_NAME\
+     --net=host \
+     -e DISPLAY \
+     --device=/dev/input \
+     -v ${HOME}/.Xauthority:/home/user/.Xauthority \
+     $DOCKER_IMAGE_NAME
